3af4cfcef0ef0562e77d810c66691dbb5916e377
Suchy <kontakt@suchego.pl>
2013-12-14 17:00:01 +0100

    Add redirect from results if there was no answers.

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -1,4 +1,13 @@
 'use strict';
 app.controller('ResultsController', ['$scope', '$location', '$rootScope', function ($scope, $location, $rootScope) {
 
+
+  if(!angular.isUndefined($rootScope.answers)) {
+    // here should some smart actions
+    // to calculate personality
+    delete $rootScope.answers;
+  } else {
+    $location.path('/').replace();
+  }
+
 }]);

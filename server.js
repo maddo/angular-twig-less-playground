736a32e9b6899d9407814f22901faac9cb9c3052
Suchy <kontakt@suchego.pl>
2014-01-04 16:37:10 +0100

    Move from apache/php to node.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express  = require('express'),
+    app = express();
+
+if(app.settings.env === 'development') {
+  app.use(express.static(__dirname + '/.tmp'));
+  app.use(express.logger('dev'));
+} else if(app.settings.env === 'production') {
+  app.use(express.static(__dirname + '/dist'));
+}
+
+app.use(function(req, res) {
+  return res.redirect(req.protocol + '://' + req.get('Host') + '/#' + req.url);
+});
+
+app.listen(process.env.PORT || 5000);
+
+console.log('ChessPersonality started!');

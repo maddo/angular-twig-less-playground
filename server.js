4404ec4605554377f169aebc0ca4f5142d0723e8
Suchy <kontakt@suchego.pl>
2014-01-04 20:19:54 +0100

    Change app url for heroku.

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(function(req, res) {
 
   var ua = req.headers['user-agent'];
 
+  console.log(ua);
+  
   if(crawlers.indexOf(ua) !== -1){
     res.sendfile(__dirname + '/tmp/snapshots/' + req.path.replace(/\//g, '_') + '.html');
   } else {

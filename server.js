f3463c73eea0441da7106eefd5cc408c2544fdff
Suchy <kontakt@suchego.pl>
2014-01-04 17:40:48 +0100

    Configure heroku.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express  = require('express'),
     app = express();
 
-// app.use(require('prerender-node'));
+app.use(require('prerender-node'));
 
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/tmp'));
 
-// app.use(function(req, res) {
-//   return res.redirect(req.protocol + '://' + req.get('Host') + '/#' + req.url);
-// });
+app.use(function(req, res) {
+  return res.redirect(req.protocol + '://' + req.get('Host') + '/#' + req.url);
+});
 
 var port = process.env.PORT || 5000;
 
